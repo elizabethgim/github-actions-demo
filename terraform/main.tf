4a69ecd3ad9c0ad2eb8d8263b2716dfb78f68ce6
Yujin <65852282+elizabethgim@users.noreply.github.com>
2025-12-25 00:42:01 +0900

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,16 @@
 terraform {
+
   backend "s3" {
-    bucket = "cas3135-2025-tfstates"
-    key = "cas3135-2021147608/calc-k8s"
+    bucket         = "cas3135-2025-tfstates"
+    key            = "cas3135-2024148030/calc-k8s.tfstate"
+    region         = "ap-northeast-2"
     dynamodb_table = "cas3135-terraform-locks"
-    region = "ap-northeast-2"
-    encrypt = true
+    encrypt        = true
   }
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.29"
-    }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
     }
   }
   required_version = ">= 1.5.0"
@@ -23,30 +20,34 @@ provider "kubernetes" {
   config_path = var.kubernetes_config_path
 }
 
-data "aws_ecr_authorization_token" "ecr_token" {
-}
-
-resource "kubernetes_secret" "aws_ecr_cred" {
-	metadata {
-		name = "aws-ecr-cred"
-		namespace = "calc"
-	}
-	type = "kubernetes.io/dockerconfigjson"
-	data = {
-		".dockerconfigjson" = jsonencode({
-			auths = {
-				"${data.aws_ecr_authorization_token.ecr_token.proxy_endpoint}" = {
-					username = data.aws_ecr_authorization_token.ecr_token.user_name
-					password = data.aws_ecr_authorization_token.ecr_token.password
-					auth = data.aws_ecr_authorization_token.ecr_token.authorization_token
-				}
-			}
-		})
-	}
-}
-
 resource "kubernetes_namespace" "ns" {
   metadata {
     name = "calc"
   }
 }
+
+# ECR 로그인 토큰 (임시)
+data "aws_ecr_authorization_token" "ecr_token" {}
+
+# k8s secret: ECR private registry pull credentials
+resource "kubernetes_secret" "aws_ecr_cred" {
+  metadata {
+    name      = "aws-ecr-cred"
+    namespace = "calc"
+  }
+
+  type = "kubernetes.io/dockerconfigjson"
+
+  data = {
+    ".dockerconfigjson" = jsonencode({
+      auths = {
+        "${data.aws_ecr_authorization_token.ecr_token.proxy_endpoint}" = {
+          username = data.aws_ecr_authorization_token.ecr_token.user_name
+          password = data.aws_ecr_authorization_token.ecr_token.password
+          auth     = data.aws_ecr_authorization_token.ecr_token.authorization_token
+        }
+      }
+    })
+  }
+}
+
